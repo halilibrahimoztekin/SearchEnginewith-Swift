df732292dee3aeaad33c5852565d2ef062448044
Halil Ibrahim Oztekin <35626045+halilibrahimoztekin@users.noreply.github.com>
2021-11-07 16:24:11 +0300

    Initial Commit

---
+++ MovieReviewAppwithSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieReviewAppwithSwift
+//
+//  Created by Halil İbrahim Öztekin on 7.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
